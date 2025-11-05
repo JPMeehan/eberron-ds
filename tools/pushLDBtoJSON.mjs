ac890cce5fe23808a650b897ac04be3fddc66433
Joseph <JPMeehan5@Gmail.com>
2025-11-04 23:17:26 -0800

    Build script adjustments

--- tools/pushLDBtoJSON.mjs
+++ tools/pushLDBtoJSON.mjs
@@ -1,6 +1,5 @@
 import { extractPack } from "@foundryvtt/foundryvtt-cli";
 import { promises as fs } from "fs";
-import path from "path";
 
 const SYSTEM_ID = process.cwd();
 const yaml = false;
@@ -16,7 +15,6 @@ for (const pack of packs) {
     `${SYSTEM_ID}/src/packs/${pack}`,
     {
       yaml,
-      transformName,
       transformEntry,
       expandAdventures,
       folders,
@@ -24,20 +22,6 @@ for (const pack of packs) {
       omitVolatile: true,
     },
   );
-}
-/**
- * Prefaces the document with its type.
- * @param {object} doc - The document data.
- */
-function transformName(doc, context) {
-  const safeFileName = doc.name.replace(/[^a-zA-Z0-9А-я]/g, "_");
-  // If we add adventures and enable "expand adventures" it will need handling
-  const type = doc._key?.split("!")[1];
-
-  const prefix = ["actors", "items"].includes(type) ? doc.type : type;
-  let name = `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${yaml ? "yml" : "json"}`;
-  if (context.folder) name = path.join(context.folder, name);
-  return name;
 }
 
 /**
@@ -52,7 +36,7 @@ async function transformEntry(entry) {
     lastModifiedBy: null,
   });
   // Remove module flags
-  for (const key of Object.keys(entry.flags)) if (!["core", "draw-steel"].includes(key)) delete entry.flags[key];
+  for (const key of Object.keys(entry.flags)) if (!["core", "draw-steel", "eberron-ds"].includes(key)) delete entry.flags[key];
 
   // Fix ownership (folders don't have ownership)
   if (entry.ownership) entry.ownership = { default: 0 };
@@ -65,17 +49,5 @@ async function transformEntry(entry) {
         if (e["effects"]) for (const grandchild of e["effects"]) Object.assign(grandchild._stats, { lastModifiedBy: null });
       }
     }
-  }
-
-  if (entry._key !== "!journal!2OWtCOMKRpGuBxrI") return;
-
-  for (const jep of entry.pages) {
-    const docsPath = path.join("src", "docs", jep.flags["draw-steel"].wikiPath);
-
-    // re-route in-game asset links to wiki image links
-    const mdContent = jep.text.markdown.replaceAll("systems/draw-steel/assets/docs", "https://github.com/MetaMorphic-Digital/draw-steel/blob/develop/assets/docs");
-
-    await fs.writeFile(docsPath, mdContent, { encoding: "utf8" });
-    jep.text = { format: 2 };
   }
 }
